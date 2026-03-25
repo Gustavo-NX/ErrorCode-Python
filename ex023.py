59c8f40f53c4569692193fb3688ea71fa45e4a28
Gustavo Silva <gustavosilva.enge@gmail.com>
2026-03-24 21:12:05 -0300

    Tentando desafio 23 e completando 22

---
+++ ex023.py
@@ -0,0 +1,24 @@
+#programa que leia um número de 0 a 9999 e mostre na tela cada um dos digitos separados. ex: 1834 (unidade 4 , dezena 3 ,centea: 8, milhar: 1)
+
+# numero = int(input('Escreva um número entre 0 a 9999: '))
+# num = str(numero)
+
+# if numero < 1000:
+#     num[0] = str(0)
+# elif numero < 100:
+#     num[1] = str(0)
+# elif numero < 10:
+#     num[2] = str(0)
+# else:
+#     print('Digite um núemro válido')
+
+# print('')
+# print(f"""  DADOS  
+#       Número escolhido: {numero}
+      
+#       Milhar: {num[0]}
+#       Centena: {num[1]}
+#       Dezena: {num[2]}
+#       Unidade: {num[3]}""")
+
+#Em progresso
