96610bf6148ef61f9d29027f3cab65dc2cf1a202
Gustavo Silva <gustavosilva.enge@gmail.com>
2026-04-07 19:09:06 -0300

    Efetuando exercicio 30

---
+++ ex030.py
@@ -0,0 +1,9 @@
+#leia numero inteiro e mostre se é par ou impar
+
+numero = int(input('Digite um núemro: '))
+print('')
+
+if numero % 2 == 0:
+    print('Este numero é par')
+else:
+    print('Este número é impar')
