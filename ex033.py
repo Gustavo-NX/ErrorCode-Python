83add459732cbb2ff108f8b93bb41a2bd580fe47
Gustavo Silva <gustavosilva.enge@gmail.com>
2026-04-14 19:21:02 -0300

    Melhorando exercicios 33 e 35

--- ex033.py
+++ ex033.py
@@ -1,22 +1,11 @@
 #leia três números e diga qual o maior e qual o menor
-from random import randint
 
 lista = []
 for i in range(3):
     numero = int(input(f'Digite o {i+1}° valor: '))
     lista.append(numero)
 
-
-if lista[0] > lista[1]:
-    lista[0], lista[1] = lista[1], lista[0]
-    if lista[1] > lista[2]:
-        lista[2], lista[1] = lista[1], lista[2]
-        if lista[0] > lista[1]:
-            lista[0], lista[1] = lista[1], lista[0]
-elif lista[0] < lista[1] > lista[2]:
-    lista[2], lista[1] = lista[1], lista[2]
-    if lista[0] > lista[1]:
-        lista[0], lista[1] = lista[1], lista[0]
+lista.sort()
 
 print('')
 print(f'Ordem menor para o maior: {lista[0]} {lista[1]} {lista[2]}')
--- ex035.py
+++ ex035.py
@@ -1,3 +1,13 @@
 #Um programa que leia o comprimento de três retas e diga ao usuârio se ele pode ou não formar um triângulo
 
-#EM PROGRESSO
+lista = []
+for i in range(3):
+    numero = int(input(f'Digite o valor da {i+1}° reta: '))
+    lista.append(numero)
+
+if lista[0] + lista[1] > lista[2] and \
+   lista[0] + lista[2] > lista[1] and \
+   lista[1] + lista[2] > lista[0]:
+    print('As três retas juntas formam um triângulo!!')
+else:
+    print('Não é possível formar um triângulo!')
